1972dab626be743ce90411b8db0c3d5c88d29434
DssDab <36041322+DssDab@users.noreply.github.com>
2023-11-02 15:45:37 +0900

    Update Title_Mgr.cs

--- Assets/Scripts/Title_Mgr.cs
+++ Assets/Scripts/Title_Mgr.cs
@@ -5,13 +5,24 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
+float blinkTime =0.0f;
+public Text TitleText;
 public class Title_Mgr : MonoBehaviour
 {
     public Text GameStartText;
     // Update is called once per frame
     void Update()
     {
+     TitleText.text = "시작하려면 아무키나 누르세요";
+     if(blinkTime >=0.5f)
+     {
+     
+     }
+     blinkTime += Time.deltaTime;
+     if
         if (Input.anyKeyDown)
             SceneManager.LoadScene("GameScene");
+            
     }
+    
 }
